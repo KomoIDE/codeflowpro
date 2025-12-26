650981c6eaf426a9076bed8f2b30ce3aebbc4358
KomoIDE <kimecube@gmail.com>
2025-12-26 11:01:30 +0600

    Add rollupOptions to vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -18,6 +18,9 @@ export default defineConfig({
 
   build: {
     target: 'es2022',
+    rollupOptions: {
+      external: ['@react-router/cloudflare'],
+    },
   },
   esbuild: {
     target: 'es2022',
